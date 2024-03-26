1353442b309c5ab006f696fc6ef457da642e09d5
Maia Federica_G <maiafedericag@gmail.com>
2024-03-25 22:17:34 -0300

    Instale extensiones como dotenv, morgan, moongose,etc.Ademas seleccione un puerto para correr y configure los middlewares para identificar el puerto en el que estoy

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
 
-console.log("hola mundo")
+const app = express();
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=>{
+    console.info("Estoy en el puerto "+ app.get("port"))
+})
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
