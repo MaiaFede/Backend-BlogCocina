7b18423b29c0eda8c6de3dbbb521f15a035f373d
Maia Federica_G <maiafedericag@gmail.com>
2024-03-25 23:38:25 -0300

    Agregue la funcion get id y put editar recetas

--- src/controllers/receta.controllers.js
+++ src/controllers/receta.controllers.js
@@ -24,3 +24,33 @@ export const crearReceta = async(req, res)=>{
      res.status(400).json({mensaje:'Error al buscar las recetas'})
     }
    };
+
+   export const obtenerReceta = async(req, res) => {
+    try {
+ console.info(req.params.id)
+     const recetaBuscada = await Tarea.findById(req.params.id)
+     if (!recetaBuscada){
+        return res.status(404).json({mensaje:" El id enviado NO corresponde a ninguna de las recetas"})
+     }
+     res.status(200).json(recetaBuscada)
+    } catch (err) {
+     console.error(err);
+     res.status(400).json({mensaje:'Error al obtener las recetas'})
+    }   
+};
+
+export const editarReceta = async(req, res) => {
+    try {
+    
+     const recetaBuscada = await Receta.findById(req.params.id)
+    
+     if (!recetaBuscada){
+        return res.status(404).json({mensaje:"El id enviado NO corresponde a ninguna de las recetas"})
+     }
+    await Receta.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({mensaje:" La receta fue editada correctamente"})
+    } catch (err) {
+     console.error(err);
+     res.status(500).json({mensaje:'Error al editar las recetas'})
+    }
+   };
--- src/routes/receta.routes.js
+++ src/routes/receta.routes.js
@@ -1,8 +1,8 @@
 import {Router} from "express"
-import { crearReceta, listarRecetas } from "../controllers/receta.controllers.js";
+import { crearReceta, listarRecetas, obtenerReceta , editarReceta} from "../controllers/receta.controllers.js";
 
 const router = Router()
 
 router.route('/recetas').get(listarRecetas).post(crearReceta)
-
+router.route("/recetas/:id").get(obtenerReceta).put(editarReceta)
 export default router
